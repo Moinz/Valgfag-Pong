320cde4f31cd2d8726c8628ca91be3fef2cf0a61
DESKTOP-QING0H4\Moin <defoose@gmail.com>
2017-11-20 20:13:22 +0100

    Initializing the project.

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[RequireComponent( typeof (Rigidbody) )]
+public class PaddleController : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    public float Speed;
+	// Use this for initialization
+	void Start ()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float yVelocity = Input.GetAxis("Vertical") * Speed;
+        _rigidbody.velocity = new Vector3(0, yVelocity, 0);
+	}
+}
